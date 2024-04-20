6be15c306ca0d3bcbb1c883308889a1e4355d594
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-04-20 11:41:51 +0300

    SVELTE - fist step! Component. Import.Export like a .svelte. Using variables and other.

---
+++ src/lib/dialog.svelte
@@ -0,0 +1,11 @@
+<dialog class="modal" open>
+	<div>
+		<button class="close">X</button>
+		<form method="dialog" class="modalForm">
+			<input type="text" name="name" placeholder="name" />
+			<input type="text" name="sourname" placeholder="sourname" />
+			<input type="number" name="age" placeholder="age" />
+			<button type="submit">Submit</button>
+		</form>
+	</div>
+</dialog>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Dialog from '../lib/dialog.svelte';
+	const name = 'Uladzimir';
+	const alt = 'Cat in glasses and shirt';
+	const src_0 =
+		'https://gratisography.com/wp-content/uploads/2024/01/gratisography-cyber-kitty-800x525.jpg';
+	const src =
+		'https://t4.ftcdn.net/jpg/02/16/28/19/360_F_216281970_6gotBzdxtFD6vjh7RGmcc4X2JpJz3pr0.jpg';
+</script>
+
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+<h1>It's Me - {name.toUpperCase()}!!!</h1>
+<img {src} alt="FREE" />
+<img src={src_0} {alt} />
+<Dialog />
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
